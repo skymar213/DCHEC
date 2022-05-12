0f28c7a5f497a5015749760b925ad913672b91f4
dell <m.boulanouar@esi-sba.dz>
2022-05-12 22:43:43 +0100

    1

--- app/src/main/java/com/example/dchec/MessageFragment.java
+++ app/src/main/java/com/example/dchec/MessageFragment.java
@@ -1,21 +1,104 @@
 package com.example.dchec;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MessageFragment extends Fragment {
+    private RecyclerView recycler;
+    private ArrayList<User> users;
+    private ProgressBar progressBar;
+    private usersAdapter usersAdapter;
+    usersAdapter.onUserClickListener onUserClickListener;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private String myImageUrl;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.message_fragment , container , false);
+
+        progressBar =(ProgressBar) view.findViewById(R.id.progressBar);
+        users = new ArrayList<>();
+        recycler = (RecyclerView) view.findViewById(R.id.recycler);
+        swipeRefreshLayout = view.findViewById(R.id.swipeLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getUsers();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+    onUserClickListener = new usersAdapter.onUserClickListener() {
+        @Override
+        public void onUserClicked(int position) {
+            startActivity(new Intent(getActivity(),MessageFragment.class)
+                    .putExtra("nom_of_roommate", users.get(position).getNom())
+                    .putExtra("prenom_of_roommate", users.get(position).getPrenom())
+                    .putExtra("email_of_roommate",users.get(position).getEmail())
+                    .putExtra("image_of_roommate",users.get(position).getProfilePicture())
+                    .putExtra("my_image",myImageUrl)
+
+            );
+
+        }
+    };
+    getUsers();
         return view;
-    }
-
-
 }
+    private void getUsers(){
+        users.clear();
+        FirebaseDatabase.getInstance().getReference("User").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    users.add(dataSnapshot.getValue(User.class));
+                }
+                usersAdapter = new usersAdapter(users,getContext(),onUserClickListener);
+                recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+                recycler.setAdapter(usersAdapter);
+                progressBar.setVisibility(View.GONE);
+                recycler.setVisibility(View.VISIBLE);
+
+                for(User user:users){
+                    if(user.getEmail().equals(FirebaseAuth.getInstance().getCurrentUser().getEmail())){
+                        myImageUrl = user.getProfilePicture();
+                        return;
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/dchec/usersAdapter.java
@@ -0,0 +1,72 @@
+package com.example.dchec;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+
+
+
+import java.util.ArrayList;
+
+public class usersAdapter extends RecyclerView.Adapter<usersAdapter.usersHolder> {
+
+    private ArrayList<User> users;
+    private Context context;
+    private onUserClickListener onUserClickListener;
+
+    public usersAdapter(ArrayList<User> users, Context context, usersAdapter.onUserClickListener onUserClickListener) {
+        this.users = users;
+        this.context = context;
+        this.onUserClickListener = onUserClickListener;
+    }
+
+    interface onUserClickListener {
+        void onUserClicked(int position);
+    }
+
+
+    @NonNull
+    @Override
+    public usersHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.user_holder, parent, false);
+        return new usersHolder(view);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull usersHolder holder, int position) {
+        holder.txtUsername.setText(users.get(position).getNom()+""+users.get(position).getPrenom());
+        Glide.with(context).load(users.get(position).getProfilePicture()).error(R.drawable.profile_icon).placeholder(R.drawable.profile_icon).into(holder.imgProfile);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    class usersHolder extends RecyclerView.ViewHolder {
+        TextView txtUsername;
+        ImageView imgProfile;
+
+        public usersHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onUserClickListener.onUserClicked(getAdapterPosition());
+                }
+            });
+            txtUsername = itemView.findViewById(R.id.txtUsername);
+            imgProfile = itemView.findViewById(R.id.imgProfile);
+
+        }
+
+    }
+}
