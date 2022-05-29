a3335603be02deef0633b8559ae6ebb223a14b16
dell <m.boulanouar@esi-sba.dz>
2022-05-29 15:32:37 +0100

    1

--- app/src/main/java/com/example/dchec/ProfileFragment.java
+++ app/src/main/java/com/example/dchec/ProfileFragment.java
@@ -5,19 +5,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,16 +31,19 @@ public class ProfileFragment extends Fragment {
     TextView txtUsername, txtPhoneNum;
     Button btnContacter;
     RecyclerView recyclerPost;
-    DatabaseReference userRef,postsRef;
+
+    DatabaseReference userRef;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.profile_fragment , container , false);
+        View view = inflater.inflate(R.layout.profile_fragment, container, false);
 
         recyclerPost = view.findViewById(R.id.recyclerPosts);
+        recyclerPost.setLayoutManager(new LinearLayoutManager(getContext()));
+        DisplayAllUserPosts();
         txtPhoneNum = view.findViewById(R.id.txtPhoneNum);
         txtUsername = view.findViewById(R.id.txtUsername);
         btnContacter = view.findViewById(R.id.btnContacter);
@@ -47,7 +55,7 @@ public class ProfileFragment extends Fragment {
         userRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
+                if (snapshot.exists()) {
                     String username = snapshot.child("userName").getValue().toString();
                     txtUsername.setText(username);
 
@@ -63,40 +71,84 @@ public class ProfileFragment extends Fragment {
         });
 
 
-
-
-
-
-
-        return view ;
+        return view;
     }
 
 
-    public void getPost(){
+    private void DisplayAllUserPosts() {
 
-        List<Posts>data = new ArrayList<>();
+        FirebaseRecyclerOptions<Posts> options = new FirebaseRecyclerOptions.Builder<Posts>().setQuery(FirebaseDatabase.getInstance().getReference().child("profilePosts"), Posts.class).build();
 
-        postsRef = FirebaseDatabase.getInstance().getReference().child("profilePosts").child(
-                FirebaseAuth.getInstance().getCurrentUser().getUid()
-        );
-
-        postsRef.addValueEventListener(new ValueEventListener() {
+        FirebaseRecyclerAdapter<Posts, ProfileFragment.UserPostViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Posts, UserPostViewHolder>(options) {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()){
-                    String title = snapshot.child("title").getValue().toString();
-                    String proPic = snapshot.child("postImage").getValue().toString();
+            protected void onBindViewHolder(@NonNull ProfileFragment.UserPostViewHolder holder, int position, @NonNull Posts model) {
 
-                }
+                holder.setUserName(model.getUserName());
+                holder.setPost_Image(model.getPostImage());
+                holder.setTitle(model.getTitle());
+                holder.setPrice(model.getPrice());
+
+
             }
 
+            @NonNull
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+            public ProfileFragment.UserPostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(getActivity()).inflate(R.layout.posts_view_holder, parent, false);
+                return new ProfileFragment.UserPostViewHolder(view);
             }
-        });
+        };
+
+        firebaseRecyclerAdapter.startListening();
+        recyclerPost.setAdapter(firebaseRecyclerAdapter);
+    }
+    public static class UserPostViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+
+        public UserPostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+
+        }
+
+        public void setUserName(String user_Name){
+            TextView userName = mView.findViewById(R.id.txtUsername);
+            userName.setText(user_Name);
+        }
+
+        public void setTitle(String title){
+            TextView userName = mView.findViewById(R.id.txtPostTitle);
+            userName.setText(title);
+        }
+
+        public void setPrice(String price){
+            TextView userPrice = mView.findViewById(R.id.txtPrice);
+            userPrice.setText(price);
+
+        }
+
+        public void setPost_Image( String post_Image){
+            ImageView PostImage = mView.findViewById(R.id.imgPost);
+            Picasso.get().load(post_Image).error(R.drawable.post_img).placeholder(R.drawable.post_img).into(PostImage);
+        }
+
+
 
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        DisplayAllUserPosts();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        DisplayAllUserPosts();
+    }
+
 
 
 }
